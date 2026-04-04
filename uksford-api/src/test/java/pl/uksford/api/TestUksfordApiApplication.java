3c2411cdc456b9b87620a3a2f2da8defa8b5d9e0
Kamiltlusty <k.tlusty@student.uksw.edu.pl>
2026-04-04 17:04:49 +0200

    Firstly spring boot default classes: UksfordApiApplicationTests.java, TestcontainersConfiguration.java, have been deleted. Secondly in TestcontainersInitializer.java class PostgreSQL container was set with its properties. In initialize method container is started next occurs execution of user-init.sql file and next relevant values are set for the context environment, on the end flyway migrations are manually run. The purpose of this class is to provide global container prepared to use in integration tests. Thirdly DatabaseConfigurationIT.java class has been annotated with annotations: @Transactional annotation to roll back changes made inside tests, @ContextConfiguration annotation to retrieve container preprepared for tests. Tests comprises connection with database and check whether schema has been created by checking proper initialization of enum type academic_degree.

--- uksford-api/src/test/java/pl/uksford/api/TestUksfordApiApplication.java
+++ uksford-api/src/test/java/pl/uksford/api/TestUksfordApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestUksfordApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.from(UksfordApiApplication::main).with(TestcontainersConfiguration.class).run(args);
+        SpringApplication.from(UksfordApiApplication::main).run(args);
     }
 
 }
